7d820d424043d769f5c2574aa035e78836bee994
wilman <wporto@unal.edu.co>
2025-09-02 17:46:36 -0500

    pausa

---
+++ estructuras.py
@@ -0,0 +1,19 @@
+# funciones
+# diseñar una encuesta para 6 personas 
+# nombre 
+# carrera 
+# idea de proyecto
+
+
+nombres=["kevin,stiven,wilman"]
+carrera=["ingenieria electronica, ingenieria elctronica, ingenieria mecatronica"]
+proyecto=["sistema de gestion para una tienda, no sabe, no sabe"]
+
+def programa(): 
+   nombre= input("ingresar nombre: ")
+   nombres.append(nombres)
+   carr= input("ingrasar carrera: ")
+   carrera.append(carrera)
+   proye= input("ingresar idea: ")
+   proyecto.apppend(proyecto)
+programa()
