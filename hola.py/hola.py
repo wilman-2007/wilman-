851a0084271efef864e3fcc60f6bdd6f59c4d4f4
wilman <wporto@unal.edu.co>
2025-09-09 17:44:16 -0500

    pausa

---
+++ hola.py/hola.py
@@ -0,0 +1,27 @@
+# hacer al perro feliz
+class perro:
+    def _init_(self,edad,nombre,color):
+        self.edad = edad
+        self.nombre = nombre
+        self.color = color
+    def comer(self):
+        print(f"mi perro {self.nombre} esta comiendo... ")
+
+    def jugar(self):
+        print(f"mi perro {self.nombre} esta jugando...")    
+
+    def acariciar(slef):
+        print(f"estoy acriando a mi perrro {self.nombre}")    
+
+
+
+# creando objetos
+mi_perro = perro(2, "firulais", "negro")
+print(f"mi perro se llama {self.nombre}, tiene {self.edad} años y es de color {self.color}")
+
+
+
+        
+
+
+
